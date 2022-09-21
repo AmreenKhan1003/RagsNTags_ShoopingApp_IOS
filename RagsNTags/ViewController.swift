e162adce167b6b4d05f3313938ad8b74ea3f7ab6
AmreenKhan1003 <amreen10032000@gmail.com>
2022-09-21 00:52:12 -0700

    local authentication

--- RagsNTags/ViewController.swift
+++ RagsNTags/ViewController.swift
@@ -6,14 +6,63 @@
 //
 
 import UIKit
+import LocalAuthentication
+
+class AuthenticateUser{
+    //MARK: Face ID Authentication
+    func authenticateUserByFace(){
+        let context = LAContext()
+        var authError: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &authError){
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Authenticate to access app") { success, error in
+                DispatchQueue.main.async{
+                    if success{
+                        //callAlert
+                        print("Successfully Authenticated")
+                    }
+                    else{
+                        //call alert
+                        print("Failed")
+                    }
+                }//End of dispatch
+            }
+        }//End of if
+    }//End of func
+    //MARK: Passcode Authentication
+    func authenticateUserByPasscode(){
+        let context = LAContext()
+        var authError: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthentication, error: &authError){
+            context.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: "Authenticate to access app") { success, error in
+                DispatchQueue.main.async{
+                    if success{
+                        //callAlert
+                        print("Successfully Authenticated")
+                    }
+                    else{
+                        //call alert
+                        print("Failed")
+                    }
+                }//End of dispatch
+            }
+        }//End of if
+    }//End of func
+}
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //authenticateUserByFace()
+        let auth = AuthenticateUser()
+        auth.authenticateUserByPasscode()
     }
-
+    
+    
+    
 
 }
 
