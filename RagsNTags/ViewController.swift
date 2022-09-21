e8a498ae33cf5343eead2c4f7516a92edd53c8ec
AmreenKhan1003 <amreen10032000@gmail.com>
2022-09-20 22:30:16 -0700

    Initial Commit

---
+++ RagsNTags/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RagsNTags
+//
+//  Created by Capgemini-DA322 on 9/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
